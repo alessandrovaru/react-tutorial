322c952cc4bbc9223c3b3727b81f09db7072b318
alessandrovaru <alessandrovaru@gmail.com>
2021-02-22 21:37:02 -0400

    Mostrando en la consola el estado y de suerte...

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,9 +1,12 @@
 import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
+import React, { useState } from 'react';
+
 
 
 const FilterableProductTable = ({ products }) => {
-    
+    const [filterText, inStockOnly] = useState({filterText: '', inStockOnly: false})
+    console.log([filterText, inStockOnly]);
     return (
         <div className="filter-container">
             <SearchBar />
--- src/components/ProductRow.js
+++ src/components/ProductRow.js
@@ -1,9 +1,14 @@
 
 const ProductRow = ({ product }) => {
+    const name = product.stocked ? //if in the next line
+    product.name : //else in the next line
+        <span style={{color: 'red'}}>
+            {product.name}
+        </span>
     return (
         <>
             <tr>
-                <td>{product.name}</td>
+                <td>{name}</td>
                 <td>{product.price}</td>
             </tr>
         </>
